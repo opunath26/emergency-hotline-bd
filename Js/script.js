a15bbafe4a1b0d08a439338bda17955d0d23e11e
OpuTheCoder <artistop26@gmail.com>
2025-08-29 15:57:47 +0600

    added card data

---
+++ Js/script.js
@@ -0,0 +1,67 @@
+// card data
+const services = [
+    {
+    name: "National Emergency Number",
+    helped: "National Emergency",
+    number: "999",
+    category: "All",
+    icon: "emergency.png",
+  },
+  {
+    name: "Police Helpline Number",
+    helped: "Police",
+    number: "999",
+    category: "Police",
+    icon: "police.png",
+  },
+  {
+    name: "Fire Service Number",
+    helped: "Fire Service",
+    number: "999",
+    category: "Fire",
+    icon: "fire-service.png",
+  },
+  {
+    name: "Ambulance Helpline Number",
+    helped: "Ambulance",
+    number: "1994-999999",
+    category: "Health",
+    icon: "ambulance.png",
+  },
+  {
+    name: "Women & Child Helpline",
+    helped: "Women & Child Helpline",
+    number: "109",
+    category: "Help",
+    icon: "emergency.png",
+  },
+  {
+    name: "Anti-Corruption Helpline",
+    helped: "Anti-Corruption",
+    number: "106",
+    category: "Government",
+    icon: "emergency.png",
+  },
+  {
+    name: "Electricity Helpline",
+    helped: "Electricity Outage",
+    number: "16216",
+    category: "Electricity",
+    icon: "ambulance.png",
+  },
+  {
+    name: "Brac Helpline",
+    helped: "Brac",
+    number: "16445",
+    category: "NGO",
+    icon: "brac.png",
+  },
+  {
+    name: "Bangladesh Railway Helpline",
+    helped: "Bangladesh Railway",
+    number: "163",
+    category: "Travel",
+    icon: "Bangladesh-Railway.png",
+  },
+  
+];
