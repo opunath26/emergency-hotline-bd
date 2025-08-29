48e447f1db715c51cb3917119e56be8f5e8d5089
OpuTheCoder <artistop26@gmail.com>
2025-08-29 16:59:15 +0600

    Dynamically cards added in this page

--- Js/script.js
+++ Js/script.js
@@ -33,21 +33,21 @@ const services = [
     helped: "Women & Child Helpline",
     number: "109",
     category: "Help",
-    icon: "emergency.png",
+    icon: "womenAndChild.png",
   },
   {
     name: "Anti-Corruption Helpline",
     helped: "Anti-Corruption",
     number: "106",
     category: "Government",
-    icon: "emergency.png",
+    icon: "Anti-Corruption.png",
   },
   {
     name: "Electricity Helpline",
     helped: "Electricity Outage",
     number: "16216",
     category: "Electricity",
-    icon: "ambulance.png",
+    icon: "Electricity.png",
   },
   {
     name: "Brac Helpline",
@@ -65,3 +65,35 @@ const services = [
   },
   
 ];
+
+// Generate Cards dynamically
+services.forEach((service) => {
+  const card = document.createElement("div");
+  card.classList.add(
+    "bg-white",
+    "p-4",
+    "rounded",
+    "shadow",
+    "flex",
+    "flex-col",
+    "justify-between"
+  );
+
+  card.innerHTML = `
+        <div class="flex justify-between items-center mb-2">
+            <div>
+    <img src="../assets/${service.icon}" alt="${service.name}" class="h-10 w-10">
+</div>
+            <button class="heart-btn text-red-500 text-xl"><img class="h-6 w-6" src="../assets/heartborder.png" alt="Heart"></button>
+        </div>
+        <h4 class="font-bold text-lg font-mono">${service.name}</h4>
+        <p class="font-light">${service.helped}</p>
+        <p class="font-bold text-xl mt-4">${service.number}</p>
+        <span class="badge badge-primary my-2 bg-slate-200 p-3 rounded-full opacity-80 w-fit mb-4">${service.category}</span>
+        <div class="flex gap-3 justify-between mt-2">
+            <button class="copy-btn btn btn-sm btn-outline flex items-center border-2 w-full justify-center p-2 rounded-lg"><img class="h-6 w-6" src="../assets/copyicon.png" alt="Copy"> Copy</button>
+            <button class="call-btn btn btn-sm btn-outline w-full flex items-center justify-center p-2 rounded-lg bg-green-500 text-white"><img class="h-6 w-6" src="../assets/call-icon.png" alt="Call">Call</button>
+        </div>
+    `;
+  cardsContainer.appendChild(card);
+});
